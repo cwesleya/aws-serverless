b5b0bdaaed848f45cbf7fbc566be3acd549b2843
Wesley Chambers <c.wesleya@gmail.com>
2020-07-24 11:27:05 -0500

    python3 script which will be a lambda function

---
+++ upload-personal-site-lambda.py
@@ -0,0 +1,19 @@
+import boto3
+import zipfile
+import mimetypes
+
+from io import BytesIO
+
+s3 = boto3.resource('s3')
+
+personal_bucket = s3.Bucket('personal.wesleychambers.com')
+build_bucket = s3.Bucket('personalbuild.wesleychambers.com')
+
+personal_zip = BytesIO()
+build_bucket.download_fileobj('personalbuild.zip', personal_zip)
+
+with zipfile.ZipFile(personal_zip) as myzip:
+    for nm in myzip.namelist():
+        obj = myzip.open(nm)
+        personal_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+        personal_bucket.Object(nm).Acl().put(ACL='public-read')
