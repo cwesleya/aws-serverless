edefa0a23a01ca10d4c1b73456ccc7d7590dd92b
Wesley Chambers <c.wesleya@gmail.com>
2020-07-27 12:04:41 -0500

    connect lambda to codepipeline

--- upload-personal-site-lambda.py
+++ upload-personal-site-lambda.py
@@ -10,14 +10,29 @@ def lambda_handler(event, context):
     sns = boto3.resource('sns')
     topic = sns.Topic('arn:aws:sns:us-east-2:584503411096:deployPersonalSiteTopic')
 
+    location = {
+        'bucketName': 'personalbuild.wesleychambers.com',
+        'objectKey': 'personalbuild.zip'
+    }
+
     try:
+        job = event.get('CodePipeline.job')
+
+        if job:
+            for artifact in job['data']['inputArtifacts']:
+                if artifact['name'] == 'Build':
+                    location = artifact['location']['s3Location']
+                    break
+
+        print('Building portfolio from ' + str(location))
+
         s3 = boto3.resource('s3')#, config=Config(signature_version='s3v4'))
         personal_bucket = s3.Bucket('personal.wesleychambers.com')
-        build_bucket = s3.Bucket('personalbuild.wesleychambers.com')
+        build_bucket = s3.Bucket(location['bucketName'])
 
         # # This is for in memory storage
         personal_zip = BytesIO()
-        build_bucket.download_fileobj('personalbuild.zip', personal_zip)
+        build_bucket.download_fileobj(location['objectKey'], personal_zip)
 
         with zipfile.ZipFile(personal_zip) as myzip:
             for nm in myzip.namelist():
@@ -37,6 +52,11 @@ def lambda_handler(event, context):
         print('Job done!')
 
         topic.publish(Subject='Deployment', Message='Personal site deployed!')
+
+        if job:
+            codepipeline = boto3.client('codepipeline') # try resource
+            codepipeline.put_job_success_result(jobId=job['id'])
+
     except:
         topic.publish(Subject='Deployment failed', Message='Personal site was not successfully deployed.')
         raise
